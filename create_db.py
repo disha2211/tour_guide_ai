62ba1ba676d7cc5d73b57d3c3da508504d6612ed
Disha Kaushal <dishakaushal22@gmail.com>
2026-05-22 14:10:04 +0530

    skeleton for workflow

---
+++ create_db.py
@@ -0,0 +1,13 @@
+# new chromadb client
+
+#create images_collection
+
+#for each category
+
+#add images to images_collection
+
+#load documents
+
+#documents to chunks
+
+#add chunks to documents_collection
