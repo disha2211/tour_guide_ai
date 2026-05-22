9d312e07d33d6bb4a915809f814b0f573be1328e
Disha Kaushal <dishakaushal22@gmail.com>
2026-05-22 14:26:35 +0530

    added chromadb client

--- create_db.py
+++ create_db.py
@@ -1,4 +1,14 @@
+from utils import ImageEmbeddings,DATA_PATH,DB_PATH
+
+import chromadb
+
+
 # new chromadb client
+
+#-------------------as working on local machine , just need a persistent client
+#------------------check the docs under chroma clients (persistent client)
+client_db = chromadb.PersistentClient(path=DB_PATH)
+
 
 #create images_collection
 
