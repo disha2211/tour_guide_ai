7b618f6a2125f05ae36bed9bfac6b43385d87ea9
Disha Kaushal <dishakaushal22@gmail.com>
2026-05-25 12:36:10 +0530

    return text and meta data

--- query_data.py
+++ query_data.py
@@ -22,18 +22,34 @@ def classify_img(client_db,query_img):
 
     return results['ids'][0][0].split('-')[0]
 
+def get_most_similar_chunks(client_db, query_question, img_category):
+	collection = client_db.get_collection(name=f"documents_{img_category}")
+
+	results = collection.query(
+		query_texts=[query_question],
+		n_results=3
+	)
+
+    #print(results)
+    #return text and meta data
+
+	return results['documents'][0], results['metadatas'][0]
+
+
+
 
 #input data: query img, query text
 query_img = None # path to image
-query_question = None
+query_question = None  # "when was it built?"
 
 #create chroma db client
 #-----------similar to client created in create_db.py
 client_db = chromadb.PersistentClient(path=DB_PATH)
 
 #classify image
-
+img_category=classify_img(client_db,query_img)
 
 #get most similar chunks
+chunks=get_most_similar_chunks(client_db,query_question)
 
 #create response
